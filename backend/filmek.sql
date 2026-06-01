db436c2f167adffc7c88dc1b35a2005149f03c15
anyadpitaja <bereczkiferenc1117@gmail.com>
2026-06-01 14:27:59 +0200

    Update filmek.sql

--- backend/filmek.sql
+++ backend/filmek.sql
@@ -20,7 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Adatbázis: `filmek`
 --
-
+CREATE DATABASE IF NOT EXISTS `filmek` DEFAULT CHARACTER SET utf8 COLLATE utf8_hungarian_ci;
+USE `filmek`;
 -- --------------------------------------------------------
 
 --
